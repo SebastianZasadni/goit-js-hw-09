ba77862f8cbbc29c496794fc808741ab06c6ed21
SebastianZasadni <112890476+SebastianZasadni@users.noreply.github.com>
2023-02-23 19:00:42 +0000

    Deploying to gh-pages from @ SebastianZasadni/goit-js-hw-09@a924fb10381b6ce526b20e4e3bd511f5a8c56a19 🚀

---
+++ 02-timer.664f3365.js
@@ -0,0 +1,2 @@
+const e=new Date,t=e.getHours()+":"+e.getMinutes();console.log("getDate():"+t);
+//# sourceMappingURL=02-timer.664f3365.js.map
---
+++ 02-timer.cf6da39d.js
@@ -0,0 +1,2 @@
+!function(){var e=new Date,t=e.getHours()+":"+e.getMinutes();console.log("getDate():"+t)}();
+//# sourceMappingURL=02-timer.cf6da39d.js.map
