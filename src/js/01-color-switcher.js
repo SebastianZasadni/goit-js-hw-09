a1c5c56339d8965fc287dee35742a16901ce93db
SebastianZasadni <emblelele@gmail.com>
2023-02-23 18:57:32 +0100

    feat task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = 0;
+
+buttonStart.addEventListener('click', () => {
+        buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    timerId = setInterval(() => {
+                body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+
+});
 
