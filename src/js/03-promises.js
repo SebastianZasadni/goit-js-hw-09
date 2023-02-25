000cfa0a1ab7a0e05ff649d543e3521c33492f76
SebastianZasadni <emblelele@gmail.com>
2023-02-25 19:46:24 +0100

    feat task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
+function createPromise(position, delayStep) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delayStep}ms`);
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delayStep}ms`);
   }
-}
+};
+
+const form = document.querySelector('form');
+
+const handleSubmit= (event) => {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+    let i = 0;
+    let position = parseInt(delay.value);
+    let delayStep = parseInt(step.value);
+        timerId = setInterval(() => {
+        if (i < amount.value){
+        i += 1;
+        console.log("I = " + i + " Start = " + position + " Step = " + delayStep);
+        createPromise(position, delayStep);
+        position += delayStep;         
+        }
+        else{
+        clearInterval(timerId);
+        }
+        }, step.value);
+ 
+    
+ };
+form.addEventListener('submit', handleSubmit);
+
+
+  
