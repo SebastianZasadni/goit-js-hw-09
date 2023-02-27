31c2e2e29bf7a6ca1787ccb2fd7767a432def740
SebastianZasadni <emblelele@gmail.com>
2023-02-27 13:41:22 +0100

    fix task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,14 @@
 import Notiflix from 'notiflix';
 
-function createPromise(position, delayStep) {
+const createPromise = (position, delayStep) => {
   const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
   if (shouldResolve) {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delayStep}ms`);
+    resolve("success value");
   } else {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delayStep}ms`);
+    reject("error");
   }
+})
 };
 
 const form = document.querySelector('form');
@@ -16,23 +18,31 @@ const handleSubmit= (event) => {
   const {
     elements: { delay, step, amount }
   } = event.currentTarget;
+  
     let i = 0;
-    let position = parseInt(delay.value);
+    let position = parseInt(amount.value);
     let delayStep = parseInt(step.value);
-        timerId = setInterval(() => {
-        if (i < amount.value){
-        i += 1;
-        createPromise(position, delayStep);
-        position += delayStep;         
-        }
-        else{
-        clearInterval(timerId);
-        }
-        }, step.value);
- 
-    
+    let firstDelay = parseInt(delay.value);
+        
+         
+    timerIdMain = setInterval(() => {
+      createPromise(position, firstDelay)
+            .then(user => Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${firstDelay}ms`))
+            .catch(error => Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${firstDelay}ms`));
+            i += 1;
+            timerIdSecond = setInterval(() => {
+              if (i < position){
+                i += 1;
+                createPromise(position, delayStep)
+                .then(user => Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${firstDelay}ms`))
+                .catch(error => Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${firstDelay}ms`));
+                firstDelay += delayStep;}             
+             else{
+                clearInterval(timerIdSecond);}
+                  }, delayStep);
+            clearInterval(timerIdMain);
+    }, firstDelay);
+               
  };
 form.addEventListener('submit', handleSubmit);
 
-
-  
